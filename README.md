02fb4cf5b22d03864874a04ab184a703d4bb3ba8
2TallTyler <tyler@tylertrahan.com>
2021-01-11 15:27:43 -0500

    Add SJ X2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# x2000_tutorial
-A NewGRF tutorial for OpenTTD
+# SJ X2
+
+![Trains passing in a field](/docs/sj_x2.PNG)
+
+An entry into the [TT-Forums NewGRF Competition](https://www.tt-forums.net/viewtopic.php?f=26&t=88258) for Jan/Feb 2021.
---
+++ render_command.py
@@ -0,0 +1,28 @@
+#################################
+####### SET UP FILES HERE #######
+#################################
+
+# Name of NewGRF, as it appears in file names
+vehicle_name = "x2000_1_cab"
+
+vehicle_manifest = "manifest"
+
+#################################
+# NO NEED TO CHANGE STUFF BELOW #
+#################################
+
+import subprocess
+import shutil
+import os
+
+input_voxel = "voxel/" + vehicle_name + ".vox"
+manifest_path = "voxel/manifest/" + vehicle_manifest + ".json"
+output_sprite = "src/gfx/" + vehicle_name
+
+output_sprite_path = output_sprite + "_8bpp.png"
+
+if os.path.exists(output_sprite_path):
+    os.remove(output_sprite_path)
+
+gorender = subprocess.run(["C:/tools/gorender/renderobject.exe", "-input", input_voxel, "-m", manifest_path, "-output", output_sprite, "-8", "-palette", "C:/tools/gorender/files/ttd_palette.json"], stdout = subprocess.PIPE, text=True)
+print(gorender.stdout)
